1a32c0f9481a448e2dd3887426433a88fce22f43
mcochi <coro.dri@hotmail.com>
2020-03-28 23:00:37 +0100

    Incorpora Acceso a uno de los métodos mediante Token. Habría que poder extenderlo al resto de rutas creadas y guardar las contraseñas en la base de datos para no tenerlo en texto plano

--- ejercicionest/src/app.controller.ts
+++ ejercicionest/src/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+/*
+* En este caso le estamos indicaando que queremos utilizar la estrategia
+* local de la biblioteca passport y nos limitamos a devolver al usuarios
+* que nos mandan
+*/
+
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guards'
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+              private authService: AuthService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+//It launches LocalAuthGuard wich call Local Strategy which peforms logic authentification
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  // Me he quedado en Default Strategy:
+  // https://docs.nestjs.com/techniques/authentication
 }
--- ejercicionest/src/app.module.ts
+++ ejercicionest/src/app.module.ts
@@ -3,10 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
 import {MongooseModule} from '@nestjs/mongoose';
+//import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [ApiModule,
-  MongooseModule.forRoot('mongodb://localhost:27017/exampledb?readPreference=primary&appname=MongoDB%20Compass&ssl=false',{ useNewUrlParser: true, useUnifiedTopology: true }),],
+  MongooseModule.forRoot('mongodb://localhost:27017/exampledb',{ useNewUrlParser: true, useUnifiedTopology: true }),
+  AuthModule,
+  UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
